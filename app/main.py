c4b670d77ad66b03925ffc609920e825e8ff1c0c
DenisSedovEd <deanisik@gmail.com>
2025-12-02 10:43:13 +0700

    create main app

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from test-task!")
+from fastapi import FastAPI
+from app.api.routers import router as api_router
+from app.core.db import init_db
+
+app = FastAPI()
+
+app.include_router(api_router)
 
 
-if __name__ == "__main__":
-    main()
+@app.on_event("startup")
+async def startup():
+    await init_db()
+
+
+@app.get("/")
+async def health_check():
+    return {"status": "ok"}
